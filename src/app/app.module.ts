3e888e33178f647e108ba7bbaa1cfe415e0eb999
Niamh <niamhgallagher1@yahoo.co.uk>
2021-04-20 16:26:08 +0100

    Adding HTML Client to get data from json files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage';
@@ -20,7 +21,8 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
     IonicStorageModule.forRoot({
       name: '_doggi-walks',
       driverOrder: ['indexeddb', 'sqlite', 'websql', 'localstorage']
-    })
+    }),
+    HttpClientModule
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
