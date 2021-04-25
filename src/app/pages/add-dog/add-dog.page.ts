fa3665070851d3c22a43d70b7b3fdfefde91393a
Niamh <niamhgallagher1@yahoo.co.uk>
2021-04-25 22:21:12 +0100

    Refactoring code and adding functionality for the data to be added to the database if the user is logged in

--- src/app/pages/add-dog/add-dog.page.ts
+++ src/app/pages/add-dog/add-dog.page.ts
@@ -1,8 +1,10 @@
+import { DogService } from './../../services/dog/dog.service';
+import { LoadingService } from './../../services/loading/loading.service';
 import { Storage } from '@ionic/storage';
 import { DogInfo } from './../../model/DogInfo';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { LoadingController, NavController, ToastController } from '@ionic/angular';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-dog',
@@ -19,10 +21,66 @@ export class AddDogPage implements OnInit {
     private http: HttpClient,
     private storage: Storage,
     private navCtrl: NavController,
-    private toastCtrl: ToastController
+    private toastCtrl: ToastController,
+    private loadingServ: LoadingService,
+    private dogServ: DogService
   ) { }
 
   ngOnInit() {
+    this.resetDogInfo();
+    this.http.get('./assets/dog-breeds.json').subscribe((breeds: any) => {
+      this.dogBreeds = breeds.breeds;
+    });
+  }
+
+  addDog() {
+    this.loadingServ.presentLoading();
+    this.http.get('https://dog.ceo/api/breeds/image/random').subscribe((res: any) => {
+    this.dogInfo.image = res.message;
+    this.storage.get('user_info').then((user) => {
+      if(user) {
+        this.dogServ.addDog(this.dogInfo).then(async (res) => {
+          console.log('added', res);
+          this.dogServ.getDogs().subscribe((dogs) => this.allDogs = dogs);
+          console.log('uploaded dog', this.allDogs);
+          this.navCtrl.pop();
+          this.resetDogInfo();
+          this.loadingServ.dismissLoading();
+          const toast = this.toastCtrl.create({
+            message: this.dogInfo.name + ' has been added!',
+            position: 'bottom',
+            duration: 2000,
+            color: 'success'
+          });
+          (await toast).present();
+        }).catch((error) => console.error('error', error));
+      } else {
+        this.storage.get('dogList').then((dogs) => {
+          if(dogs) {
+            this.allDogs = dogs;
+          }
+            this.dogInfo.id = this.allDogs.length;
+            this.allDogs.push(this.dogInfo);
+            this.storage.set('dogList', this.allDogs).then(async () => {
+              console.log('uploaded dog', this.allDogs);
+              this.navCtrl.pop();
+              this.resetDogInfo();
+              this.loadingServ.dismissLoading();
+              const toast = this.toastCtrl.create({
+                message: this.dogInfo.name + ' has been added!',
+                position: 'bottom',
+                duration: 2000,
+                color: 'success'
+              });
+              (await toast).present();
+            });
+          });
+        }
+      });
+    });
+  }
+
+  resetDogInfo() {
     this.dogInfo = {
       id: null,
       name: '',
@@ -33,33 +91,6 @@ export class AddDogPage implements OnInit {
       notes: '',
       image: ''
     }
-    this.http.get('./assets/dog-breeds.json').subscribe((breeds: any) => {
-      this.dogBreeds = breeds.breeds;
-    });
-  }
-
-  addDog() {
-    this.storage.get('dogList').then((dogs) => {
-      if(dogs) {
-        this.allDogs = dogs;
-      }
-      this.http.get('https://dog.ceo/api/breeds/image/random').subscribe((res: any) => {
-        this.dogInfo.image = res.message
-        this.dogInfo.id = this.allDogs.length;
-        this.allDogs.push(this.dogInfo);
-        this.storage.set('dogList', this.allDogs).then(async () => {
-          console.log('uploaded dog', this.allDogs);
-          this.navCtrl.pop();
-          const toast = this.toastCtrl.create({
-            message: this.dogInfo.name + ' has been added!',
-            position: 'bottom',
-            duration: 2000,
-            color: 'success'
-          });
-          (await toast).present();
-        });
-      })
-    });
   }
 
 }
