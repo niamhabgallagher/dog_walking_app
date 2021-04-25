4be54895ade7eb345b623db9a503d687de600081
Niamh <niamhgallagher1@yahoo.co.uk>
2021-04-25 20:59:13 +0100

    Updating view user page to show user info if logged in

--- src/app/pages/view-user/view-user.page.ts
+++ src/app/pages/view-user/view-user.page.ts
@@ -1,6 +1,9 @@
+import { User } from './../../model/User';
+import { Storage } from '@ionic/storage';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-view-user',
@@ -9,12 +12,19 @@ import { NavController } from '@ionic/angular';
 })
 export class ViewUserPage implements OnInit {
 
+  userInfo: User;
+
   constructor(
     private navCtrl: NavController,
-    private router: Router
+    private storage: Storage,
+    private afAuth: AngularFireAuth
   ) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.updateUserInfo();
   }
 
   login() {
@@ -25,5 +35,27 @@ export class ViewUserPage implements OnInit {
   register() {
     console.log('register nav');
     this.navCtrl.navigateForward('/tabs/viewuser/register');
+  }
+
+  logout() {
+    this.afAuth.signOut().then(async (res) => {
+      console.log('logged out', res);
+      await this.storage.set('user_info', undefined);
+      await this.storage.set('token', undefined);
+      this.updateUserInfo();
+    }).catch((error) => {
+      console.error('error', error);
+    });
+  }
+
+  updateUserInfo() {
+    this.storage.get('user_info').then((user) => {
+      if(user) {
+        this.userInfo = user;
+        console.log('user', this.userInfo);
+      } else {
+        this.userInfo = undefined;
+      }
+    });
   }
 }
