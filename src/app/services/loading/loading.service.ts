097132e38fd17ea78a2fb879de4885b39afb5e0b
Niamh <niamhgallagher1@yahoo.co.uk>
2021-04-25 20:56:13 +0100

    Adding loading service

---
+++ src/app/services/loading/loading.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+
+  loading: boolean = false;
+
+  constructor(
+    public loadingController: LoadingController
+  ) { }
+
+  async presentLoading() {
+    if(this.loading == false) {
+      const loading = await this.loadingController.create({
+        message: 'loading...'
+      });
+      await loading.present().then(() => this.loading = true);
+  
+      console.log('present loading!');
+    }
+  }
+
+  dismissLoading() {
+    if(this.loading == true) {
+      this.loadingController.dismiss().then(() => this.loading = false);
+      console.log('loading dismissed');
+    }
+  }
+
+
+}
+
