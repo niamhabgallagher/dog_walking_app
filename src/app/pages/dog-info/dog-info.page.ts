ab93dbf9b94edd677d0dc06cf7f29209accb6c82
Niamh <niamhgallagher1@yahoo.co.uk>
2021-04-26 14:29:48 +0100

    Adding option to edit dog notes on viewing sog info page

--- src/app/pages/dog-info/dog-info.page.ts
+++ src/app/pages/dog-info/dog-info.page.ts
@@ -1,3 +1,4 @@
+import { DogService } from './../../services/dog/dog.service';
 import { DogInfo } from './../../model/DogInfo';
 import { InfoService } from './../../services/info/info.service';
 import { Component, OnInit } from '@angular/core';
@@ -12,11 +13,18 @@ export class DogInfoPage implements OnInit {
   dog: DogInfo;
 
   constructor(
-    private info: InfoService
+    private info: InfoService,
+    private dogServ: DogService
   ) { }
 
   ngOnInit() {
     this.dog = this.info.dogInfo;
+  }
+
+  notesChange() {
+    this.dogServ.updateDog(this.dog, this.dog.id).then((change) => {
+      console.log('changes made', change, this.dog.notes);
+    });
   }
 
 }
