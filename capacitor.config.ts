5697be9fd94b2cd297afd579a0ae1254b0ae80e5
Kieran Uddin <kbu@gloversure.co.uk>
2021-07-06 21:26:52 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Doggi Walks',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
